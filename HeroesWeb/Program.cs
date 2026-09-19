1c79ec9004f05fc6dbf081399d336064e225322a
sergioagulerapantoja-ctrl <316250586+sergioagulerapantoja-ctrl@users.noreply.github.com>
2026-09-19 07:50:56 -0400

    Add ASP.NET Core Identity authentication

--- HeroesWeb/Program.cs
+++ HeroesWeb/Program.cs
@@ -1,16 +1,37 @@
 using HeroesWeb.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var connectionString = builder.Configuration.GetConnectionString("HeroesDb")
+    ?? throw new InvalidOperationException("Falta la conexión HeroesDb.");
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Heroes");
+    options.Conventions.AuthorizeFolder("/SuperPoderes");
+});
 
 builder.Services.AddDbContext<HeroesContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("HeroesDb")
-        ?? throw new InvalidOperationException(
-            "Falta la conexión HeroesDb.")));
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+
+        options.Password.RequiredLength = 8;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -26,6 +47,7 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
